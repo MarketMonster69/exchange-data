b1d084c1735d9851120c3e5ccf49564b6ac4422b
Market Monster <marketmonster69@gmail.com>
2022-11-25 22:53:12 -0500

    makes it work as proof of concept

--- exchange_data/bybit.py
+++ exchange_data/bybit.py
@@ -2,10 +2,17 @@
 from pybit import usdt_perpetual
 import time, random
 import pandas as pd
+import os
 
 
 session_unauth = usdt_perpetual.HTTP(endpoint="https://api.bybit.com")
+symbols = ["BTCUSDT"]
+intervals = ["M", "W", "D", "240", "60", "30"]
 
+#kline download path
+default_data_dir = os.path.dirname(os.getcwd()) + "/datasets"
+if not os.path.exists(default_data_dir):
+    os.makedirs(default_data_dir)
 
 def get_symbols():
 
@@ -32,28 +39,31 @@ def kline_to_csv(symbols, intervals):
         for interval in intervals:
 
             startTime = 1
-            df_kline = []
+            f_name = symbol + "_" + interval + ".csv"
+            f_path = os.path.join(default_data_dir, f_name)
 
             while True:
 
                 kline_data = session_unauth.query_kline(
                     symbol = symbol,
                     interval = interval,
-                    startTime=startTime
+                    from_time=startTime
                 )
 
-                if kline_data is None:
+                if kline_data["result"] is None:
                     break
 
-                df_kline = pd.concat(df_kline, pd.DataFrame(kline_data["result"]))
-                startTime = df_kline["open_time"].iat[-1] + 1
+                df = pd.DataFrame(kline_data["result"])
+                df.to_csv(f_path, mode='a', index=False, header=False)
+
+                startTime = df["open_time"].iat[-1] + 1
                 
                 time.sleep(random.uniform(3, 5))
-
-            df_kline.to_csv(symbol + interval + ".csv")
 
     return
 
 def kline_to_mongodb(symbols, intervals):
     return
+
+kline_to_csv(symbols, intervals)
 

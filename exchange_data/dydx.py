49c2b28368a4ffca66284869eb1156d74ec1d01a
Market Monster <marketmonster69@gmail.com>
2022-12-10 19:05:58 -0500

    adds basic get_candles method to dydx module

--- exchange_data/dydx.py
+++ exchange_data/dydx.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 
+from dydx3 import Client
+import dydx3.constants as consts
+import pandas as pd
+
 class BarDownloader:
 
-    def placeholder(self):
+    def __init__(self):
+        self.client = Client(
+            host='https://api.dydx.exchange'
+        )
         pass
-    
+
+    def get_candles(self):
+        market = consts.MARKET_BTC_USD
+        candles = self.client.public.get_candles(
+        market=market,
+        resolution='1DAY',
+        )
+
+        return pd.DataFrame(candles.data['candles'])
+
+if __name__ == "__main__":
+    bd = BarDownloader()
+    print(bd.get_candles())
